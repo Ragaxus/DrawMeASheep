57ef2f554d2a72c0221efb56192278f9decb245c
Ragaxus <ragaxus@gmail.com>
2018-06-03 15:05:54 -0500

    Fixed canvas initialization

--- public/javascripts/sheep.js
+++ public/javascripts/sheep.js
@@ -1,10 +1,10 @@
-$(document).ready(function(){
+initCanvas = function(){
     canvas=document.getElementById("myCanvas")
     ctx=canvas.getContext('2d')
     sheepImg=document.getElementById("imgSheep")
     ctx.clearRect(0,0,canvas.width,canvas.height)
     ctx.drawImage(sheepImg,0,0)
-});
+};
 
 $(document).on('click', '#myCanvas', function(e){
     canvas=document.getElementById("myCanvas")
